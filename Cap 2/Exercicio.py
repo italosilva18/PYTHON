baedf2eabf4fc869b351d2a87dc6cd668bcd6b12
italosilva18 <italosilva14@hotmail.com>
2022-07-30 14:57:30 -0300

    Atualização

--- Cap 2/Exercicio.py
+++ Cap 2/Exercicio.py
@@ -3,3 +3,39 @@ print("Resultado:",10+20*30)
 print("Resultado:",4**2/30)
 print("Resultado:",(9**4+2)*6-1)
 print("Resultado:",10%3*10**2+1-10*4/2)
+
+#PROGRAMA 2.1 (Primeiro programa com Variaveis)
+a=2
+b=3
+print(a+b)
+
+#PROGRAMA 2.2 (Cálculo de aumemnto de Salário)
+print("Cálculo de aumemnto de 5% Salário")
+salario=1500
+aumento=5
+print("Novo Salario:",salario+(salario*aumento/100))
+
+# Exercicio 2.3 Faça um programa que exiba seu nome na tela.
+print("Exercixio 2.3 [cap2]")
+Nome="Italo da silva costa" 
+print(Nome)
+
+# Exercicio 2.4 Escreva um programa que exiba o resultado de 2a * 3b, em que (a) vale (3) 
+# e (b) vale 5 
+print("Exercixio 2.4 [cap2]")
+a=3
+b=5
+print(2*a*3*b)
+# Exercicio 2.5 Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+print("Exercixio 2.5 [cap2]")
+a=10
+b=35
+c=8
+print(a+b+c)
+# Exercicio 2.6 Modifique o programa 2.2, de forma que ele calcule um aumento de 15% para um salario de R$ 750.
+print("Exercixio 2.3 [cap2]")
+salrio=750
+aumento=15
+print(750+(750*15/100))
+
+

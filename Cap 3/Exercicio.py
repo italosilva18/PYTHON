b7bb5f0b3dc53cd10039936ea0ad000a5f192a8b
italosilva18 <italosilva14@hotmail.com>
2022-07-30 19:58:55 -0300

    Criação

---
+++ Cap 3/Exercicio.py
@@ -0,0 +1,8 @@
+# Exercicio 3.1 - Complete a taabela a seguir, marcando inteiro ou flutuante dependando do numero apresentado 
+print( "Exercicio 3.1")
+print("5 int")
+print("5.0 float")
+print("4.3 float")
+print("-2 int")
+print("100 int")
+print("1.333 float")

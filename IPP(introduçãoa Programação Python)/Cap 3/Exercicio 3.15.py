59dbb80550c00ab2f2802dfba6261d30e0996543
italosilva18 <italosilva14@hotmail.com>
2022-07-31 16:19:35 -0300

    Concluido.

---
+++ IPP(introduçãoa Programação Python)/Cap 3/Exercicio 3.15.py
@@ -0,0 +1,17 @@
+#Escreva um programa para calcular a redução de tempo de vida de um fumante.Pergunte a quantidade de cigarros fumados 
+#por dia e quantos anos ele ja fumou considere que um fumante perde 10 minutos de vida a cada cigarro e calcule  
+#quantos dias de vida um fumante perderá. Exiba o total em dias
+from functools import total_ordering
+from traceback import print_last
+
+
+print( "Exercicio 3.15")
+
+cigarros = float(input("Digite a quantidade de cigarros fumados durante o dia: "))
+anos = float(input("Digite a quantidade de anos que fumou: "))
+dia = 1440
+perde1 = cigarros*10
+perde2 = anos*365
+perde3 = perde1*perde2 
+total= perde3//dia
+print(f"O fumante Perderá o total de:{total}dias")

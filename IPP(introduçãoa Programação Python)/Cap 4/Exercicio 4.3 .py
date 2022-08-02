5d8215118626afa15f2498f5c7f99aaa42628e2f
italosilva18 <italosilva14@hotmail.com>
2022-08-02 19:30:38 -0300

    concluido

--- IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.3 .py
+++ IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.3 .py
@@ -1,25 +1,21 @@
 # Escreva um programa que leia três numeros e que imprima o maior e o menor.
 print("Exercicio 4.1")
-
 numero01= int(input("Digite um numero:"))
 numero02= int(input("Digite o segundo numero:"))
 numero03= int(input("Digite o terceiro numero:"))
+maior= numero01
 
-if numero01>numero02<numero03:
-    print("O maior numero é o Primeiro:")
-    print("O menor numero é o Segundo:")
-if numero02>numero01<numero03:
-    print("O maior numero é o Segundo:")
-    print("O menor numero é o Primeiro:")
-if numero03>numero01>numero02:  
-    print("O maior numero é o Terceiro:")
-    print("O menor numero é o Segundo:")
-if numero01>numero02>numero03:
-    print("O maior numero é o Primeiro:")
-    print("O menor numero é o Terceiro:")
-if numero02>numero01>numero03:
-    print("O maior numero é o Segundo:")
-    print("O menor numero é o Terceiro:")
-if numero03>numero01<numero02:  
-    print("O maior numero é o Terceiro:")
-    print("O menor numero é o Primeiro:")
+if numero02>numero03 and numero02>numero01:
+   maior=numero02
+if numero03>numero02 and numero03>numero01:
+    maior=numero03
+
+menor= numero01
+if numero02<numero03 and numero02<numero01:
+   menor=numero02
+if numero03<numero02 and numero03<numero01:
+    menor=numero03
+
+print(f"O maior numero digitado é:{maior}")
+print(f"O menor numero digitado é:{menor}")
+

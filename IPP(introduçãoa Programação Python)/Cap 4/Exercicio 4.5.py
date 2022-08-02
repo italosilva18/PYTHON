ee2a4acfb53a2c2a1fc8f7b45d626cd59f1674e9
italosilva18 <italosilva14@hotmail.com>
2022-08-02 20:27:23 -0300

    Completo.

--- IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.5.py
+++ IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.5.py
@@ -1,5 +1,5 @@
 #execultando um prorama para saber se o carro é novo ou velho!
-
+print("Exercicio 4.5")
 idade=int(input("Digite a idae do seu veiculo: "))
 if idade<=3:
     print("Seu veiculo é novo !")
---
+++ IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.6.py
@@ -0,0 +1,10 @@
+#Escreva um programa que pergunte a distancia que um passageiro deseja percorrer em km.
+#Calcule o preço da passagem, cobrando R$0,50 por km para viagens de ate 200km e 0,45 para viagens mais longas!
+
+print("Exercicio 4.6")
+percuso=float(input("Qual é distância que deseja percorrer: "))
+if percuso>200:
+    passagem=percuso*0.45
+else:
+    passagem = percuso*0.50
+print(f"O valor da passagem é de R$:{passagem:5.2f} e a quilometragem é de {percuso}km.")    

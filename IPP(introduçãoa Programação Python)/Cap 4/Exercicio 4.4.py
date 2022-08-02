56ddddd4419d57d2d8227fac9a0f30cb1d68e81c
italosilva18 <italosilva14@hotmail.com>
2022-08-02 20:07:25 -0300

    Concluido

--- IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.4.py
+++ IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.4.py
@@ -1,6 +1,6 @@
 # Escreva um programa que pergunte o salario do funcionario e calcule o valor do aumento
 #Para salarios superiores a R$ 1.250,00 calcule um aumento de 10%.Para os inferiores ou iguais,de 15%
-
+print("Exercicio 4.4")
 salario=float(input("Digite o salario do funcionario:"))
 valor=salario
 salario1=(valor*15/100)
---
+++ IPP(introduçãoa Programação Python)/Cap 4/Exercicio 4.5.py
@@ -0,0 +1,9 @@
+#execultando um prorama para saber se o carro é novo ou velho!
+
+idade=int(input("Digite a idae do seu veiculo: "))
+if idade<=3:
+    print("Seu veiculo é novo !")
+else:
+    print("Seu veiculo e velho!")
+    
+    

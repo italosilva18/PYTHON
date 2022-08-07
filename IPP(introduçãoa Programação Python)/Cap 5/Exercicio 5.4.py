8ab6715d775296b0a08ad087b8907347917df249
italosilva18 <italosilva14@hotmail.com>
2022-08-07 11:14:27 -0300

    Atualizar!

---
+++ IPP(introduçãoa Programação Python)/Cap 5/Exercicio 5.4.py
@@ -0,0 +1,7 @@
+#fazer um programa para imprimir de 1 ate o numero digitado pelo usuariosomente nuremo impares
+
+fim= int(input("Digite um numreo: "))
+x = 1
+while x <= fim:
+    print(x)
+    x=x+2

542a9ebc782089513215aeca07e0ac5b481ef97c
italosilva18 <italosilva14@hotmail.com>
2022-08-07 22:53:00 -0300

    cd

---
+++ IPP(introduçãoa Programação Python)/Cap 5/Exercicio 5.8.py
@@ -0,0 +1,10 @@
+#Escreva um programa que leia dois numero. imprima o resultado da multiplicação do primeiro pelo segundo,
+# ultilize apenas os operadores de soma e subtração para calcular o resultad. 
+# Lembre-se de que podemos entender a multiplicação de dois numeros  como sumas sucessivas de um deles. Assim, 4x5=5+5+5+5=4+4+4+4+4
+
+num1=int(input("Digite o primeiro numero: "))
+num2=int(input("Digite o segundo numero: "))
+
+while num1<=num2:
+    print(num1=r)    
+    num1=num1+1

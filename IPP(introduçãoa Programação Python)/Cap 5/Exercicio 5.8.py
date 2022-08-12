5fc189c0f18545f9ddf33affda09dada706fc51d
italosilva18 <italosilva14@hotmail.com>
2022-08-11 21:27:27 -0300

    concluido!

--- IPP(introduçãoa Programação Python)/Cap 5/Exercicio 5.8.py
+++ IPP(introduçãoa Programação Python)/Cap 5/Exercicio 5.8.py
@@ -2,9 +2,11 @@
 # ultilize apenas os operadores de soma e subtração para calcular o resultad. 
 # Lembre-se de que podemos entender a multiplicação de dois numeros  como sumas sucessivas de um deles. Assim, 4x5=5+5+5+5=4+4+4+4+4
 
-num1=int(input("Digite o primeiro numero: "))
-num2=int(input("Digite o segundo numero: "))
-
-while num1<=num2:
-    print(num1=r)    
-    num1=num1+1
+p = int(input("Primeiro número: "))
+s = int(input("Segundo número: "))
+x = 1
+r = 0
+while x <= s:
+    r = r + p
+    x = x + 1
+print(f"{p} x {s} = {r}")

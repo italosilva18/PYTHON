0f1359d005b97287988c46d546e54873dccb2dff
italosilva18 <italosilva14@hotmail.com>
2022-08-21 21:40:48 -0300

    Concluido

---
+++ IPP(introduçãoa Programação Python)/Cap 5/Exercicio 5.11.py
@@ -0,0 +1,16 @@
+#Escreva um programa que pergunte o deposito inicial e a taxa de juros de uma poupaça 
+# Exiba os valores Mês a mês para os 24 primeiros meses Escreva o total ganhos com juros no periodo
+
+deposito=float(input("Digite o Valor de Deposito Inicial: "))
+taxa=float(input("Digite o Valor da Taxa de Juros: "))    
+juros=deposito*taxa/100
+mes=1
+saldo=deposito
+while mes<=24:
+    saldo = saldo+juros
+    print(f"Mês: {mes} é de R${saldo:5.2f}.")
+    mes=mes+1
+
+print(f"O ganho com os juros foi R$: {saldo-deposito:5.2f}.")
+    
+    
